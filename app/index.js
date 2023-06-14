0df4622eb45aa80a58f86f7c0f91b5d7ea43cfb9
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-06-14 20:18:10 +0100

    Timer put in place. Main functionalaities icomplete

--- app/index.js
+++ app/index.js
@@ -3,20 +3,19 @@ import { Clock } from "../components";
 
 import { Stack } from "expo-router";
 import { useState } from "react";
+import { Stopwatch, Timer} from 'react-native-stopwatch-timer';
 
 // we need a function to calculate how much time went from the strat of the app but also to continuisly display that time
 //maybe a state bool that keep track of the clock on, if yes then keep calling a function repeatedly 
-function timer (start){
-    let time  = Date.now();
-    return(time - start);
-}
-
 //to Add a giganctic stat button when the clock is not running 
 
+let laps = [1, 2, 3]
+ 
 const App = () => {
 
-    let time = 0;
     const [clockOn, setClockOn] = useState(false);
+    const [watchRunning, setwatchRunning] = useState(false);
+    const [resetWatch, setResetWatch] = useState(false);
 
     return(
         <SafeAreaView style={{flex: 1}}>
@@ -33,33 +32,48 @@ const App = () => {
                         setClockOn={setClockOn}
                     />
                 </View>
-                <View style={{borderWidth: 1, height: "10%", alignItems:"center"}}>
-                    <Text style={{textAlignVertical:"center", textAlign:"center"}}>{time}</Text>
+                <View style={{borderWidth: 1, height: "15%", alignItems:"center"}}>
+                    <Stopwatch 
+                        start={watchRunning}
+                        msecs={true}
+                        reset={resetWatch}
+                    ></Stopwatch>
                 </View>
                 <View style={{borderWidth:1, height:"40%"}}>
                     <ScrollView>
-                        <Text>List of all laps will be here</Text>
-                        <Text>List of all laps will be here</Text>
-                        <Text>List of all laps will be here</Text>
+                        <FlatList
+                            data={laps}
+                            renderItem={(items) => {
+                                items.forEach(element => (
+                                    <Text>{element}</Text>
+                                ))
+                            }}
+
+                        />
                     </ScrollView>
+                    <TouchableOpacity style={{alignContent: "center", alignItems:"center"}}>
+                        <Text>Lap</Text>
+                    </TouchableOpacity>
                 </View>
                 <View style={{borderWidth: 1, height:"15%", flexDirection:"row", alignItems:"center"}}>
-                    <TouchableOpacity style={{left:"10%"}}>
+                    <TouchableOpacity style={{left:"10%"}} onPress={()=>{
+                        //clear the lists of laps
+                    }}>
                         <Text>Clear</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity style={{left: "120%", position:"relative"}} 
                     onPress={()=>{
-                        setClockOn(true)
-                        setInterval(()=>{
-                            time+=1;
-                            console.log(time)
-                        }, 1000);
+                        setClockOn(true);
+                        setResetWatch(false);
+                        setwatchRunning(true);
                     }}> 
-                        <Text>Lap</Text>
+                        <Text>Start</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={{left: "250%"}} onPress={()=> {
                         setClockOn(false);
+                        setwatchRunning(false);
+                        setResetWatch(true);
                     }}>
                         <Text>Stop</Text>
                     </TouchableOpacity>
